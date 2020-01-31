b3ab7883ebf7fc64e48b4c0223ccb018bbc79941
Dylan Manley <dylan.manley@gmail.com>
2020-01-31 09:58:00 -0800

    adjust application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::API
   include Response
   rescue_from ActiveRecord::RecordNotFound do |exception|
     json_response({ message: exception.message }, :not_found)
+  end
 end
