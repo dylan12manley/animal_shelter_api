c2a950d66aab98234ef672a9ac51f911a68ba80f
Dylan Manley <dylan.manley@gmail.com>
2020-01-31 10:44:48 -0800

    update models and create response module in concerns

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 
 15.times do |i|
   @shelter = Shelter.create!(
-    shelter_name: Faker::Address.community
+    shelter_name: Faker::Address.community,
     city: Faker::Address.city,
     state: Faker::Address.state,
     address: Faker::Address.street_address,
-    phone_number: rand(9710000000)
+    phone_number: 9710
   )
   2.times do
     @shelter.animals.new(
@@ -18,26 +18,26 @@
     shelter_id: nil)
     @shelter.save
   end
-  2.times do
-    @shelter.animals.new(
-    name: Faker::Creature::Cat.name,
-    species: Faker::Creature::Animal.name,
-    breed: "unknown",
-    sex: "na",
-    age: rand(14),
-    status: "available",
-    shelter_id: nil)
-    @shelter.save
-  end
-  2.times do
-    @shelter.animals.new(
-    name: Faker::FunnyName.two_word_name,
-    species: "Dog",
-    breed: Faker::Creature::Dog.breed,
-    sex: "Male",
-    age: rand(14),
-    status: "reserved",
-    shelter_id: nil)
-    @shelter.save
-  end
+  # 2.times do
+  #   @shelter.animals.new(
+  #   name: Faker::Creature::Cat.name,
+  #   species: Faker::Creature::Animal.name,
+  #   breed: "unknown",
+  #   sex: "na",
+  #   age: rand(14),
+  #   status: "available",
+  #   shelter_id: nil)
+  #   @shelter.save
+  # end
+  # 2.times do
+  #   @shelter.animals.new(
+  #   name: Faker::FunnyName.two_word_name,
+  #   species: "Dog",
+  #   breed: Faker::Creature::Dog.breed,
+  #   sex: "Male",
+  #   age: rand(14),
+  #   status: "reserved",
+  #   shelter_id: nil)
+  #   @shelter.save
+  # end
 end
